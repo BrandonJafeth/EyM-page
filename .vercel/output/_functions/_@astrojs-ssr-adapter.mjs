a392318aa2036562e5e3b55dfb1291565d3ec650
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2026-02-11 12:47:42 -0600

    feat/cambios

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './chunks/_@astrojs-ssr-adapter_DJld9z1k.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_CrB5rYpT.mjs';
-export { renderers } from '../renderers.mjs';
