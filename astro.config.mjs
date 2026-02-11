67b85d2973697c46f723d4bbdf4d6c6dd2a8cf61
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2026-02-11 12:07:15 -0600

    FEAT/cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emyasociados.net',
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
